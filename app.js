557a9ead303a869311e0a7641bc377fc9dde9eb6
gaster7738 <110089603+CodeCat6230@users.noreply.github.com>
2022-08-05 17:03:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ var app = express()
 app.listen(3000, function(){
     console.log("Express_Server_is_completly_Open")
 })
+
+
+
+
+app.get('/', function(req,res){
+    res.send("<h1>thanks for visit my site!</h1>")
+});
