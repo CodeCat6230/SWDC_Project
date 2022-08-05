c21120bcf175fe2cf08884ebd9e927bd76452ee5
gaster7738 <110089603+CodeCat6230@users.noreply.github.com>
2022-08-05 11:40:08 +0900

    Update repeat_code.js

--- repeat_code.js
+++ repeat_code.js
@@ -1,12 +1,19 @@
- let repeat_f, j, p, q;
+let i = 0;
+let j = 0;
+let p = 0;
+let q = 0;
 
  let repeat_w = 10;
- i = 0;
 
 while (repeat_w > 0) {
-    console.log(i);
     i++;
+    console.log('i', i);
     repeat_w--;
   }
 
-//for 
+  console.log("--------");
+
+for(repeat_f = 0; repeat_f < 10; repeat_f++){
+    j++;
+    console.log("j", j);
+}
