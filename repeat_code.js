43ce79ad0554a006324879157cee3d80daebc844
gaster7738 <110089603+CodeCat6230@users.noreply.github.com>
2022-08-05 15:31:53 +0900

    Update repeat_code.js

--- repeat_code.js
+++ repeat_code.js
@@ -1,5 +1,13 @@
 let i = 0;
 let j = 0;
+
+let dog={
+  name: "뽀삐",
+  color_: "갈색",
+  kind_: "푸들",
+  age: 5
+};
+
 let p = 0;
 let q = 0;
 
@@ -18,4 +26,18 @@ while (repeat_w > 0) {
 for(repeat_f = 0; repeat_f < 10; repeat_f++){
     j++;
     console.log("j", j);
+}
+
+
+
+
+
+for (let key in dog){
+  console.log(`${key}: ${dog[key]}`)
+}
+
+
+let numbers = [25, 113, 829, 12, 24, 9, 5, 3, 29];
+for(let number of numbers){
+  console.log(number);
 }
