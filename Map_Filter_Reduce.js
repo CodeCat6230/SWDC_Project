4a7790907e79de9b6499e4261047cd8839175847
gaster7738 <110089603+CodeCat6230@users.noreply.github.com>
2022-08-05 16:08:24 +0900

    Create Map_Filter_Reduce.js

---
+++ Map_Filter_Reduce.js
@@ -0,0 +1,23 @@
+
+const array_1 = [1,2,3,4,5,6,7,8,9,0]
+
+const map_1 = array_1.map(array_1 => array_1 + 300);
+
+console.log(map_1)
+
+const song_Length = ['kjfd', 'qqhtqt', 'fiufahvkj', 'hqrkjtl', 'hgas', 'hjgs', 'hfskgjah', 'hafjgks']
+
+const Filter_1 = song_Length.filter(song_Length => song_Length.length >= 6);
+
+console.log(Filter_1);
+
+const array1 = [32, 45, 7, 6432];
+
+const IV = 0;
+const SWI = array1.reduce(
+  (PV, CV) => PV + CV,
+  IV
+);
+
+console.log(SWI);
+// expected output: 10
