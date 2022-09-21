f73496e5607ff389c5387ed6fe224275cf694c89
gaster7738 <110089603+CodeCat6230@users.noreply.github.com>
2022-09-21 12:11:16 +0900

    .

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-var express = require('express')
-
-var app = express()
-
-app.listen(3000, function(){
-    console.log("Express_Server_is_completly_Open")
-})
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
 
 
 
-
-app.get('/', function(req,res){
-    res.send("<h1>thanks for visit my site!</h1>")
+console.log("server is opend by port 3000!")
+app.get('/', (req, res) => {
+    res.send('get function is running')
+    console.log(items)
 });
+
+app.post('/',(req, res) => {
+    res.send('post function is running')
+});
+
+app.patch('/', (request, response) => {
+    response.send('patch function is running')
+});
+
+app.listen(port,() => {});
